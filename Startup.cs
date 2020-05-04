7bf7e93965afc0d726d2734e1bdcc1f92ef1368d
Anselmo Neto <anselmo.otero@gmail.com>
2020-05-04 19:04:25 -0300

    Adicionando repository pattern

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using ProductCatalog.Data;
+using ProductCatalog.Repositories;
 
 namespace ProductCatalog
 {
@@ -15,20 +13,34 @@ namespace ProductCatalog
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddResponseCompression();
+            services.AddScoped<StoreDataContext, StoreDataContext>();
+            // O AddTransient criar uma nova instancia a cada requisição, abrindo nova conexão com o DB.
+            // Por isso usamos o AddScoped que usa uma unica instancia. 
+            //services.AddTransient<StoreDataContext, StoreDataContext>();
+            services.AddTransient<ProductRepository, ProductRepository>();
+            services.AddSwaggerGen(x => {
+                x.SwaggerDoc("v1", new OpenApiInfo {Title = "Product Catalog Api", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseMvc();
+            app.UseResponseCompression();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Catalog Api - v1");
             });
+            // app.Run(async (context) =>
+            // {
+            //     await context.Response.WriteAsync("Hello World!");
+            // });
         }
     }
 }
